0e23a50665f17db5c4b570a479aefe87c02642b8
maximiliankraemer <maximilian@startup-werk.de>
2023-03-08 13:08:59 +0100

    implement products

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {ProductsService} from "../service/products.service";
+
 
 @Component({
   selector: 'app-products',
@@ -6,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent {
+
+  constructor(public products: ProductsService) {}
 
 }
---
+++ src/app/service/products.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+
+  public jerseyImages = [
+    {
+      "jersey": "assets/img/23-fastbreak-jersey-city-edition_pi4900000_ff_4900361-a62a4ef6ad028d688904_full.webp",
+      "name" : "Brooklyn Nets City Jersey - Kevin Durant",
+      "price" : "95.00 €"
+    },
+    {
+      "jersey": "assets/img/23-swingman-jersey-city-edition_pi4758000_ff_4758898-1d5778c9b6c54148e18b_full.webp",
+      "name" : "Dallas Mavericks City Jersey - Luka Doncic",
+      "price" : "95.00 €"},
+    {
+      "jersey": "assets/img/23-swingman-jersey-city-edition_pi4758000_ff_4758915-e830a7d84d7ccc6ef2ad_full.webp",
+      "name" : "Los Angeles Lakers City Jersey - Lebron James",
+      "price" : "95.00 €"},
+    {
+      "jersey": "assets/img/miami-heat-nike-city-edition-swingman-jersey-22-white-jimmy-butler-unisex_ss4_p-13366627+u-2mo5lyj54r6tmsjye87c+v-274285e9ee694e55b2a5e803cb9df043.webp",
+      "name" : "Los Angeles Lakers City Jersey - Lebron James",
+      "price" : "95.00 €"},
+    {
+      "jersey": "assets/img/golden-state-warriors-nike-city-edition-swingman-jersey-22-black-stephen-curry-unisex_ss4_p-13366656+u-149ylto68sfgw6ebgqnt+v-6376b213fa7a40e697e95d6a7beeb4de.webp",
+      "name" : "Los Angeles Lakers City Jersey - Lebron James",
+      "price" : "95.00 €"},
+    {
+      "jersey": "assets/img/memphis-grizzlies-jordan-statement-edition-swingman-jersey-light-blue-ja-morant-unisex_ss4_p-13365099+u-u4sqa1sqwunvqigwjmfg+v-232c064df42446f3b652a652f051e9ec.webp",
+      "name" : "Los Angeles Lakers City Jersey - Lebron James",
+      "price" : "95.00 €"},
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+
+
+  }
+
+
+}
+
