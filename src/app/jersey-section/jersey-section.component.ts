7e8cc29a70aa12e366ba06d1b8ca81e78079a3b5
maximiliankraemer <maximilian@startup-werk.de>
2023-03-08 13:08:21 +0100

    implement jersey section

--- src/app/jersey-section/jersey-section.component.ts
+++ src/app/jersey-section/jersey-section.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {ProductsService} from "../service/products.service";
 
 @Component({
   selector: 'app-jersey-section',
@@ -7,25 +8,11 @@ import {Component, OnInit} from '@angular/core';
 })
 export class JerseySectionComponent implements OnInit{
 
-  jerseyImages = [
-    {"jersey": "assets/img/23-fastbreak-jersey-city-edition_pi4900000_ff_4900361-a62a4ef6ad028d688904_full.webp"},
-    {"jersey": "assets/img/23-swingman-jersey-city-edition_pi4758000_ff_4758898-1d5778c9b6c54148e18b_full.webp"},
-    {"jersey": "assets/img/23-swingman-jersey-city-edition_pi4758000_ff_4758915-e830a7d84d7ccc6ef2ad_full.webp",}
-  ];
+  constructor(public products: ProductsService) { }
 
-  constructor() { }
+  ngOnInit(): void {
 
-  ngOnInit() {
-    this.getJerseyImages()
   }
-
-  getJerseyImages() {
-    return this.jerseyImages.map(jersey => {
-      console.log(jersey.jersey)
-      return jersey.jersey;
-    });
-  }
-
 
 
 }
