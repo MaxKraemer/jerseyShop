a84c1d42b5e742d1d902527a8b7c86358c84b31e
maximiliankraemer <maximilian@startup-werk.de>
2023-04-10 16:39:25 +0200

    create navbar and player container

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { MatDialogModule} from "@angular/material/dialog";
+import { FormsModule} from "@angular/forms";
+import {MatDialog} from '@angular/material/dialog';
+import {RegisterComponent} from "../register/register.component";
+import {LoginComponent} from "../login/login.component";
+import {CartService} from "../service/cart.service";
+import {AuthService} from "../service/auth.service";
+
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+
+
+
+constructor(public dialog: MatDialogModule, public matDialog: MatDialog, public cart: CartService, public auth: AuthService) {}
+
+
+  openDialog() {
+   this.matDialog.open(RegisterComponent);
+  }
+
+  openLoginDialog() {
+    this.matDialog.open(LoginComponent);
+  }
+
+  logout() {
+    this.auth.logoutUser();
+  }
+
+}
---
+++ src/app/player-container/player-container.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-player-container',
+  templateUrl: './player-container.component.html',
+  styleUrls: ['./player-container.component.css']
+})
+export class PlayerContainerComponent {
+
+}
