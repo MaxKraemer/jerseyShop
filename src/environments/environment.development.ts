a3adddb5f0bdb935fc159b28a0eef4f05a40dc50
maximiliankraemer <maximilian@startup-werk.de>
2023-04-10 16:37:25 +0200

    install firebase and customer profile

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'webshop-fdaec',
+    appId: '1:173185739847:web:ceb4890059930aa43fd562',
+    storageBucket: 'webshop-fdaec.appspot.com',
+    apiKey: 'AIzaSyB1sOsW6SVg7lMXQvZRhOl1zy8Fbt6muzs',
+    authDomain: 'webshop-fdaec.firebaseapp.com',
+    messagingSenderId: '173185739847',
+  },};
