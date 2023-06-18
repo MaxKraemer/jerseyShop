101f7de695f122e52e7f7d3cb5708d281f4c1169
maximiliankraemer <maximilian@startup-werk.de>
2023-06-18 17:23:12 +0200

    show jersey at my dashboard

--- src/app/user-dashboard/user-dashboard.component.ts
+++ src/app/user-dashboard/user-dashboard.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 import {AuthService} from "../service/auth.service";
+import { CartService } from '../service/cart.service';
+import { ProductsService } from '../service/products.service';
 
 @Component({
   selector: 'app-user-dashboard',
@@ -8,15 +11,22 @@ import {AuthService} from "../service/auth.service";
 })
 export class UserDashboardComponent {
 
-  constructor(public auth: AuthService) {}
+  constructor(public auth: AuthService, public cartService: CartService, public productService: ProductsService, public angularFirestore: AngularFirestore) {}
 
   public user: any;
+  jerseys: any[] = [];
 
   ngOnInit(): void {
     this.auth.userData().subscribe((user: any) => {
       console.log(user,'user');
       this.user = user;
     });
+    this.jerseyItems();
+  }
+
+  public jerseyItems(): void {
+    this.cartService.getItems();
+    this.jerseys = this.cartService.jerseys;
   }
 
 }
