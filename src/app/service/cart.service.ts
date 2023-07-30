b76b8f489e96378697d89214dd7edbda7e9db8a5
maximiliankraemer <maximilian@startup-werk.de>
2023-07-30 15:34:32 +0200

    new addToCart function

--- src/app/service/cart.service.ts
+++ src/app/service/cart.service.ts
@@ -4,6 +4,7 @@ import {AngularFirestore} from "@angular/fire/compat/firestore";
 import {collection, doc, Firestore, setDoc} from "@angular/fire/firestore";
 import '@firebase/firestore';
 import {Observable} from "rxjs";
+import { AuthService } from './auth.service';
 
 
 @Injectable({
@@ -14,7 +15,7 @@ export class CartService {
   jerseys: any[] = [];
   public bagdeCount = 0;
 
-  constructor(public productService: ProductsService, public firestore: Firestore, public angularFirestore: AngularFirestore) {
+  constructor(public productService: ProductsService, public firestore: Firestore, public angularFirestore: AngularFirestore, public authService: AuthService) {
     // check if database has items
     this.angularFirestore.collection('allProducts').get().subscribe((data) => {
       console.log('1 this.productService.jerseyImages', this.productService.jerseyImages);
@@ -25,16 +26,33 @@ export class CartService {
     this.bagdeCount++;
   }
 
-  addToCart(product: any) {
-    const jersey = collection(this.firestore, 'jerseys');
-    setDoc(doc(jersey), product);
-  }
+  // addToCart(product: any) {
+  //   const jersey = collection(this.firestore, 'jerseys');
+  //   setDoc(doc(jersey), product);
+  // }
 
-  getItems(): any {
-    this.angularFirestore.collection('jerseys').valueChanges().subscribe((data) => {
-      this.jerseys = data;
+  // getItems(): any {
+  //   this.angularFirestore.collection('jerseys').valueChanges().subscribe((data) => {
+  //     this.jerseys = data;
+  //   });
+  // }
+
+  public addToCart(product: any) {
+    // Get the user ID from the AuthService
+    this.authService.userData().subscribe((userData: any) => {
+      if (userData && userData.uid) {
+        const jersey = collection(this.firestore, "jerseys");
+        const cartItem = { ...product, userId: userData.uid };
+
+        setDoc(doc(jersey), cartItem).then(() => {
+          console.log("Item added to cart:", cartItem);
+        });
+      } else {
+        console.log("User not logged in");
+      }
     });
-  }
+}
+
 
   deleteJersey(id: string): any {
     this.angularFirestore.collection('jerseys').get().subscribe((data) => {
